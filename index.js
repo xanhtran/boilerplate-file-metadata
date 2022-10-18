9b5add5bef0a491bc65c0ea31425257f7410f21e
Trần Thị Xanh <75367524+xanhtran@users.noreply.github.com>
2022-10-18 16:27:07 +0700

    get name, type, size from file upload

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+var multer = require("multer");
+var upload = multer({dest: 'upload/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res)=> {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    })
+  } catch(err) {
+    res.send(400)
+  }
+})
 
 
 
